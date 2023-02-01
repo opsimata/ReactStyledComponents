8fabc568e56a152541c8d9203470eec515363dd4
Pedro <pedro.enrique1599@icloud.com>
2023-02-01 06:29:17 -0300

    first study session commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
+import { GlobalStyle } from "./Components/GlobalStyle"
 
 function App() {
   return (
     <>
-      <Cabecalho />
-      <Container />
+      <GlobalStyle/>
+      <Cabecalho/>
+      <Container/>
     </>
   );
 }
